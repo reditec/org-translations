5e8eb3a5415803a851d7b84ae928c24e593cb439
reditec <reditec@outlook.com>
2015-04-03 15:26:26 +0200

    Update account_lang.php

--- deutsch/account_lang.php
+++ deutsch/account_lang.php
@@ -5,7 +5,7 @@ $lang['account_emailaddr']    	 = "E-Mail-Adresse";
 $lang['account_showemail']    	 = "E-Mail-Adresse im Profil anzeigen";
 $lang['account_firstname']    	 = "Vorname";
 $lang['account_lastname']    	 = "Nachname";
-$lang['account_ircnick']    	 = "IRC-Nickname";
+$lang['account_ircnick']    	 = "IRC-Benutzername";
 $lang['account_nick']    		 = "Benutzername";
 $lang['account_newpasswd']    	 = "Neues Passwort";
 $lang['account_desiredpasswd']     = "Gewünschtes Passwort";
@@ -17,7 +17,7 @@ $lang['account_chooseavatar']     = "Profilbild wählen";
 $lang['account_settings']    	 = "Kontoeinstellungen";
 
 $lang['account_forgotpasswd']     = 'Passwort vergessen?';
-$lang['account_join']   	  	 = "Schließen Sie sich %d Gleichgesinnten an."; //%d in the number of users
+$lang['account_join']   	  	 = "Schließen Sie sich %d Gleichgesinnten an."; //%d is the number of users
 
 $lang['account_already_got']     = 'Haben Sie schon ein Konto?';
 
@@ -28,5 +28,5 @@ $lang['account_resend']   		 = "Erneut versenden";
 $lang['account_no_activate']    = "E-Mail nicht erhalten?";
 $lang['account_verify']   		 = "Bestätigen";
 $lang['account_verify_code']    = "Bestätigungs-Code";
-$lang['account_please_verify']    = "Bitte überprüfen Sie Ihr E-Mail-Konto nach Ihrem Bestätigungs-Code und geben Sie ihn hier ein.";
+$lang['account_please_verify']    = "Bitte durchsuchen Sie Ihren E-Mail-Posteingang nach Ihrem Bestätigungs-Code und geben Sie ihn hier ein.";
 $lang['account_verify_sent']    = "Bestätigung wurde an %s versendet"; // %s is the email address, please always include it
